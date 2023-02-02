8f81f628ce5a58061bd227bde34ad378cfbeba10
paranoiaAD <1737098665@qq.com>
2023-02-02 21:29:28 +0800

    feat: 完成基础的调试环境

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Header = () => React.createElement('h1', {
+  style: {
+    textAlign: 'center',
+    color: 'red',
+    fontSize: '1rem'
+  }
+}, 'div', <span><br />span</span>)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Header />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
